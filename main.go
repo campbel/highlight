4248cfe32e80338adf5a381b54c78df436cbd765
Chris Campbell <chrs.campbell@shopify.com>
2023-03-23 16:53:19 -0600

    detect language from filename

--- main.go
+++ main.go
@@ -7,14 +7,15 @@ import (
 	"os"
 	"strings"
 
+	"github.com/alecthomas/chroma/lexers"
 	"github.com/alecthomas/chroma/quick"
 	"github.com/campbel/yoshi"
 )
 
 type Options struct {
 	File     string `yoshi:"FILE;Input file, alternatively pass from stdin;"`
-	Language string `yoshi:"-l,--language;Language of the content;go"`
-	Theme    string `yoshi:"-t,--theme;Theme for the output;nord"`
+	Language string `yoshi:"-l,--language;Language of the content;"`
+	Theme    string `yoshi:"-t,--theme;Theme for the output;dracula"`
 	Format   string `yoshi:"-f,--format;Format of the output;terminal256"`
 }
 
@@ -24,6 +25,9 @@ func main() {
 		if content == "" {
 			fmt.Fprintln(os.Stderr, "no content to highlight")
 			os.Exit(0)
+		}
+		if options.Language == "" && options.File != "" {
+			options.Language = lexers.Match(options.File).Config().Name
 		}
 		fmt.Println(highlight(string(content), options.Language, options.Format, options.Theme))
 	})
