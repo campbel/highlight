0a47421e7db4b718ce129d2f9c7ecf8137bf1812
Chris Campbell <chrs.campbell@shopify.com>
2023-03-23 15:29:24 -0600

    just all of it

---
+++ README.md
@@ -0,0 +1,28 @@
+# Highlight
+
+Highlight code for easy viewing.
+
+![Demo of Highlight](tape/demo.gif)
+
+## Examples
+
+### Other languages
+
+```sh
+highlight -l ruby my_file.rb
+```
+
+### From stdin
+
+```sh
+cat main.go | highlight
+```
+
+### To Rich Text and Copy (Google Docs / Word)
+
+```sh
+# Output HTML and convert to rich text with textutil
+cat my_file.go | highlight -f html | textutil -stdin -format html -convert rtf -stdout | pbcopy
+```
+
+This will copy the html output and convert to rich text. You can then paste this into a document program like Word or Google Docs.
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strings"
+	"io"
+	"bytes"
+
+	"github.com/campbel/yoshi"
+	"github.com/alecthomas/chroma/quick"
+)
+
+type Options struct {
+	File string `yoshi:"FILE;Input file, alternatively pass from stdin;"`
+	Language string `yoshi:"-l,--language;Language of the content;go"`
+	Theme string `yoshi:"-t,--theme;Theme for the output;nord"`
+	Format string `yoshi:"-f,--format;Format of the output;terminal16"`
+}
+
+func main() {
+	yoshi.New("highlight").Run(func(options Options) {
+		content := getContent(options.File)
+		if content == "" {
+			fmt.Fprintln(os.Stderr, "no content to highlight")
+			os.Exit(0)
+		}
+		fmt.Println(highlight(string(content), options.Language, options.Format, options.Theme))
+	})
+}
+
+func getContent(file string) string {
+	if file == "" {
+		content, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error reading from stdin: ", err)
+			os.Exit(1)
+		}
+		return string(content)
+	}
+	content, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error reading file: ", err)
+		os.Exit(1)
+	}
+	return string(content)
+}
+
+func highlight(content, language, format, theme string) string {
+	content = strings.TrimSpace(strings.Replace(content, "\t", "    ", -1))
+	var b bytes.Buffer
+	err := quick.Highlight(&b, content, language, format, theme)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error highlighting: ", err)
+	}
+	return b.String()
+}
